a8b860467edf8403f2bc8517ea5c0072d5f06b43
Alex Beltyukov <beltyukov.1@gmail.com>
2015-04-30 18:37:23 -0500

    Add Happiness

---
+++ Happiness/Happiness/HappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Alex Beltyukov on 4/30/15.
+//  Copyright (c) 2015 Alex Beltyukov. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+    
+}
