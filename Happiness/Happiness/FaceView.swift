da3113963d75d2bd254fb4c39b2b6a362502eeda
Alex Beltyukov <beltyukov.1@gmail.com>
2015-05-28 13:06:27 -0500

    Use delegation for view to get data

--- Happiness/Happiness/FaceView.swift
+++ Happiness/Happiness/FaceView.swift
@@ -8,10 +8,18 @@
 
 import UIKit
 
+protocol FaceViewDataSource: class {
+    func smilinessForFaceView(sender: FaceView) -> Double?
+}
+
+@IBDesignable
 class FaceView: UIView {
 
+    @IBInspectable
     var lineWidth: CGFloat = 3 { didSet { self.setNeedsDisplay() } }
+    @IBInspectable
     var color = UIColor.blueColor() { didSet { self.setNeedsDisplay() } }
+    @IBInspectable
     var scale: CGFloat = 0.9 { didSet { self.setNeedsDisplay() } }
     
     var faceCenter: CGPoint {
@@ -20,6 +28,8 @@ class FaceView: UIView {
     var faceRadius: CGFloat {
         return min(bounds.size.width, bounds.size.height) / 2 * scale
     }
+    
+    weak var dataSource: FaceViewDataSource?
     
     private struct Scaling {
         static let FaceRadiusToEyeRadiusRatio: CGFloat = 10
@@ -80,7 +90,7 @@ class FaceView: UIView {
         bezierPathForEye(.Left).stroke()
         bezierPathForEye(.Right).stroke()
         
-        let smiliness = 0.0
+        let smiliness = dataSource?.smilinessForFaceView(self) ?? 0.0
         bezierPathForSmile(smiliness).stroke()
     }
 }
