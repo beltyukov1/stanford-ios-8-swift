94022f0de2abdeae6163a3a8cb20cab78c5ef9e1
Alex Beltyukov <beltyukov.1@gmail.com>
2015-06-07 16:49:24 -0500

    Add Psychologist

---
+++ Psychologist/Psychologist/PsychologistViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Psychologist
+//
+//  Created by Alex Beltyukov on 6/7/15.
+//  Copyright (c) 2015 Alex Beltyukov. All rights reserved.
+//
+
+import UIKit
+
+class PsychologistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
